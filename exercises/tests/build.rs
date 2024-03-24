0a6c33d61d3918d5d76b21be8d23a4773e0ccc11
Lzq <lzq19807572813@163.com>
2024-03-24 19:01:46 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,7 +1,8 @@
 //! This is the build script for both tests7 and tests8.
 //!
 //! You should modify this file to make both exercises pass.
-
+// use std::env;
+// use std::time::SystemTime;
 fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
@@ -10,11 +11,15 @@ fn main() {
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
-    println!("cargo:{}", your_command);
+
+    let test_foo_value = timestamp.saturating_sub(10);
+    println!("cargo:rustc-env=TEST_FOO={}", test_foo_value);
+
+    // let your_command = format!(
+    //     "Your command here with {}, please checkout exercises/tests/build.rs",
+    //     timestamp
+    // );
+    // println!("cargo:{}", your_command);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
